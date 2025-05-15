6ae37a688ed5f2665b6dae18cabea30341bfe147
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2025-05-15 15:00:59 +0200

    fix:Format project

--- src/test/java/com/orange/ussd/UssdApplicationTests.java
+++ src/test/java/com/orange/ussd/UssdApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UssdApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
